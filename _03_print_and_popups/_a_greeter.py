10cd7b7c05d661d99d18b11b21c2baf265a662c0
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-13 13:56:51 -0700

    Update _a_greeter.py

--- _03_print_and_popups/_a_greeter.py
+++ _03_print_and_popups/_a_greeter.py
@@ -4,18 +4,19 @@ from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
     
     # Make a new window variable, window = Tk()
-    
+    window_1 = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window_1.withdraw()
     # Ask the user for their name and save it to a variable
-    # name = simpledialog.askstring(title='Greeter', prompt="What is your name?")
-    
-    # Show a message box with your message using the .showinfo() method
-    
+    window_1= simpledialog.askstring(title='hi', prompt="What is your name?")
+    #window_1.askstring('Greeter', "What is your name?")
+    # Show a message box with your message using the
+    messagebox.showinfo(message='thanks')
+    #window_1.showinfo()
     # Print your message to the console using the print() function
-    
+    print(window_1)
     # Show an error message using messagebox.showerror()
+messagebox.showerror()
+# Run the window's .mainloop() method
 
-    # Run the window's .mainloop() method
-
-    pass
+pass
