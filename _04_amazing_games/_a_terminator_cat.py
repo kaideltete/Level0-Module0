dbcc4fca1c86c0a3d978774cdd9f579320beac83
kaideltete <109186541+kaideltete@users.noreply.github.com>
2022-07-13 14:27:39 -0700

    Update _a_terminator_cat.py

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -61,15 +61,19 @@ if __name__ == '__main__':
     
     # 2. Call the set_background() function with your variable inside of the parenthesis
     #    for example, set_background(bg_image)
-
+    set_background('cat-1.png')
     # 3. Make a new turtle
+
+    my_turtle = turtle.Turtle()
 
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-
+    my_turtle.color('red')
+    my_turtle.pencolor('red')
     # 5. Set the turtle width to 0 so no outlines are drawn
-
+    my_turtle.width(0)
     # 6. Set the turtle speed to 0 (fastest)
+    my_turtle.speed(0)
 
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your
@@ -80,14 +84,17 @@ if __name__ == '__main__':
     
     # 8. After you've found the x and y for the eyes create 2 eye variables
     #    and initialize them:
-    #    left_eye  = Eye(eye=my_turtle, x=-34, y=11, radius=30)
-    #    right_eye = Eye(eye=my_turtle, x=40, y=-5, radius=30)
+    left_eye  = Eye(eye=my_turtle, x=-82, y=0, radius=30)
+    right_eye = Eye(eye=my_turtle, x=93, y=-19, radius=30)
 
     # 9. Call the .draw() method on BOTH eye variables
-
+    left_eye.draw()
+    right_eye.draw()
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     window.onkeypress(key_pressed, 'space')
     window.listen()
     turtle.done()
+
+    end
